0b3d85d91f964977bf24b5b98b99e5b47486443e
karineyasmin <karine.yasmin@outlook.com>
2024-08-08 19:28:14 -0300

    updated

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,15 @@
+# flake8: noqa
+# type: ignore
+
 from django.urls import path
 from contact import views
 
 app_name = "contact"
 
 urlpatterns = [
-    path("<int:contact_id>", views.contact, name="contact"),  # type:ignore
-    path("", views.index, name="index"),  # type:ignore
+    path("search/", views.search, name="search"),
+    path("", views.index, name="index"),
+    # contact (CRUD)
+    path("contact/<int:contact_id>/", views.contact, name="contact"),
+    path("contact/create/", views.create, name="create"),
 ]
---
+++ contact/views/contact_forms.py
@@ -0,0 +1,7 @@
+from django.shortcuts import get_list_or_404, redirect, render
+from contact.models import Contact
+
+
+def create(request):
+    context = {}
+    return render(request, "contact/create.html", context)
